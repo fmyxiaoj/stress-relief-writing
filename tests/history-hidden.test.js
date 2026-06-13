ca04f79e5a43e6cdd2c735c68a89f0badf2c0e0e
fmyxiaoj <360124165@qq.com>
2026-06-14 00:30:55 +0800

    Bust history asset cache

--- tests/history-hidden.test.js
+++ tests/history-hidden.test.js
@@ -9,3 +9,10 @@ test("history list and detail are not rendered while hidden", () => {
   assert.match(css, /\.nw-history-list\[hidden\][\s\S]*display:\s*none/);
   assert.match(css, /\.nw-history-detail\[hidden\][\s\S]*display:\s*none/);
 });
+
+test("history assets use the current cache-busting version", () => {
+  const html = readFileSync(join(__dirname, "..", "index.html"), "utf8");
+
+  assert.match(html, /styles\.css\?v=20260614-history-detail/);
+  assert.match(html, /app\.js\?v=20260614-history-detail/);
+});
